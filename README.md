ea372be490f04c0d399150ac8127c7812cd914bc
Kaiofprates <kaioprates@outlook.com>
2020-08-02 19:50:18 -0300

    add readme and gif

---
+++ README.md
@@ -0,0 +1,5 @@
+# P5.js Websocket Game
+
+> This is an integration study project between [P5.js](https://p5js.org/reference/) and Socket.io
+
+<img src="./assets/intro.gif" />
--- public/sketch.js
+++ public/sketch.js
@@ -8,6 +8,9 @@ let players = {};
 var socket = io("http://localhost:3100");
 var game_id;
 
+let angle = 0;
+let slides;
+
 socket.on("init", function (id) {
   game_id = id;
   socket.on("prerender", function (data) {
@@ -22,6 +25,8 @@ socket.on("render", function (data) {
 function setup() {
   createCanvas(800, 500);
   background("#212437");
+  angleMode(DEGREES);
+  slides = createSlider(0.1, 10, 0.1, 0.1);
 }
 
 function setPlayers(c) {
@@ -34,6 +39,25 @@ function setPlayers(c) {
 function draw() {
   noStroke();
   setPlayers(players);
+  // rect(10, 250, 500, 6);
+  // rect(610, 250, 200, 6);
+
+  strokeWeight(2);
+  let x = 140 * cos(angle);
+  let y = 140 * sin(angle);
+  translate(560, 250);
+  stroke(x * 30, y * 3, 123);
+  line(x, y, 0, 0);
+  line(-x, -y, 0, 0);
+
+  fill("#6DECB1");
+  ellipse(x, y, 14);
+  ellipse(-x, -y, 14);
+
+  angle += slides.value();
+  textSize(30);
+  slides.position(400, 430);
+  text(slides.value(), 100, 740);
 }
 
 function keyPressed() {
